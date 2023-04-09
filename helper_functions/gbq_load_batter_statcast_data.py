0f0b6240ae92f87ec12bbb76480bb2ad32c2023d
John Tyler <johntyler@createdbydata.com>
2023-04-09 01:22:35 +0100

    Updated to only load from the last load date.

--- helper_functions/gbq_load_batter_statcast_data.py
+++ helper_functions/gbq_load_batter_statcast_data.py
@@ -27,11 +27,39 @@ def load_batter_data_to_gbq(daily_statcast_data_csv):
     try:
         # try to load the table
         client.get_table(table_ref)
-        # if the table already exists, delete it
-        client.delete_table(table_ref)
-        print('Table deleted.')
+        # if the table already exists print a message
+        print('Table already exists.')
+        # append the csv data to the existing table
+        job_config = bigquery.LoadJobConfig(
+            # set the source format to CSV
+            source_format=bigquery.SourceFormat.CSV,
+            # set the write disposition to WRITE_APPEND
+            write_disposition='WRITE_APPEND',
+            # set the schema
+            schema=[
+                bigquery.SchemaField("game_date", "DATE", description="The date of the game."),
+                bigquery.SchemaField("player_name", "STRING", description="The name of the player."),
+                bigquery.SchemaField("launch_speed", "FLOAT64", description="The speed of the ball when it leaves the bat."),
+                bigquery.SchemaField("launch_angle", "FLOAT64", description="The angle of the ball when it leaves the bat."),
+                bigquery.SchemaField("pitch_type", "STRING", description="The type of pitch thrown."),
+                bigquery.SchemaField("release_speed", "FLOAT64", description="The speed of the pitch when it leaves the pitcher's hand."),
+                bigquery.SchemaField("p_throws", "STRING", description="The hand of the pitcher. (L or R)")
+            ]
+        )
+        # define the path to the csv file
+        csv_file_path = 'daily_statcast_data.csv'
+        # open the csv file
+        with open(csv_file_path, 'rb') as source_file:
+            # load the csv file into the table
+            job = client.load_table_from_file(source_file, table_ref, job_config=job_config)
+            # wait for the job to complete
+            job.result()
+            # print a message
+            print('Loaded {} rows into {}:{}.'.format(job.output_rows, dataset_name, table_name))
+
+    # if the table does not exist, create it
     except:
-        # if the table does not exist, print a message
+        # print a message
         print('Table does not exist.')
 
     # create a job config object
@@ -40,15 +68,15 @@ def load_batter_data_to_gbq(daily_statcast_data_csv):
         source_format=bigquery.SourceFormat.CSV,
         # set the write disposition to WRITE_TRUNCATE
         write_disposition='WRITE_TRUNCATE',
-        # set the schema
+        # set the schema add column definitions
         schema=[
-            bigquery.SchemaField("game_date", "DATE"),
-            bigquery.SchemaField("player_name", "STRING"),
-            bigquery.SchemaField("launch_speed", "FLOAT64"),
-            bigquery.SchemaField("launch_angle", "FLOAT64"),
-            bigquery.SchemaField("pitch_type", "STRING"),
-            bigquery.SchemaField("release_speed", "FLOAT64"),
-            bigquery.SchemaField("p_throws", "STRING")
+            bigquery.SchemaField("game_date", "DATE", description="The date of the game."),
+            bigquery.SchemaField("player_name", "STRING", description="The name of the player."),
+            bigquery.SchemaField("launch_speed", "FLOAT64", description="The speed of the ball when it leaves the bat."),
+            bigquery.SchemaField("launch_angle", "FLOAT64", description="The angle of the ball when it leaves the bat."),
+            bigquery.SchemaField("pitch_type", "STRING", description="The type of pitch thrown."),
+            bigquery.SchemaField("release_speed", "FLOAT64", description="The speed of the pitch when it leaves the pitcher's hand."),
+            bigquery.SchemaField("p_throws", "STRING", description="The hand of the pitcher.")
         ]
     )
 
--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from helper_functions.get_qualified_players import get_qualified_players
 from helper_functions.get_batter_data import get_daily_statcast_data
 from helper_functions.gbq_load_batter_statcast_data import load_batter_data_to_gbq
 from helper_functions.gbq_lookup_most_recent_data import get_most_recent_date
+from datetime import date
 
 # define a list of years that will be used to get the list of qualified players
 years = [2021,2022]
@@ -20,17 +21,27 @@ qual = 300
 # find the most recent date in the existing database so we can limit our data pulls to only the required data
 most_recent_date = get_most_recent_date()
 
-# TODO: Still need to update the `gbq_load_beter_statcast_data.py` script to append the data to the existing table instead of deleting and recreating it. Additionally, to add the column definitions the first time the table is created.
-# TODO: Still Add code to check if the most recent date in the database is the same as the most recent date in the statcast data. If it is, then we don't need to run the script.
+# If the most recent date is the same as today don't run main.py. Otherwise, run main.py.
+today = date.today()
 
-# get the list of qualified players
-qualified_players = get_qualified_players(years, qual)
+if most_recent_date == today:
+    print('The most recent date in the database is the same as today. No need to run the application.')
+    exit()
+elif most_recent_date != today:
+    print('The most recent date in the database is not the same as today. Running the application.')
 
-# get the daily statcast data for each player in the list of qualified players
-daily_statcast_data = get_daily_statcast_data(qualified_players, most_recent_date)
+    # get the list of qualified players
+    qualified_players = get_qualified_players(years, qual)
 
-# write the daily statcast data to a csv file, removing the header row
-daily_statcast_data_upload = daily_statcast_data.to_csv('daily_statcast_data.csv', index=False, header=False)
+    # get the daily statcast data for each player in the list of qualified players
+    daily_statcast_data = get_daily_statcast_data(qualified_players, most_recent_date)
 
-# load daily_statcast_data.csv to BigQuery using the load_batter_data_to_gbq function from helper_functions/load_batter_data_to_gbq.py
-load_batter_data_to_gbq(daily_statcast_data_upload)
+    # write the daily statcast data to a csv file, removing the header row
+    daily_statcast_data_upload = daily_statcast_data.to_csv('daily_statcast_data.csv', index=False, header=False)
+
+    # load daily_statcast_data.csv to BigQuery using the load_batter_data_to_gbq function from helper_functions/load_batter_data_to_gbq.py
+    load_batter_data_to_gbq(daily_statcast_data_upload)
+    exit()
+else:
+    print('Something went wrong. Please check the code.')
+    exit()
